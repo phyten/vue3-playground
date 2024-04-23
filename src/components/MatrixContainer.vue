758a4edf796ec4151b5e499649f4821b096aa69b
Mikiyasu Obara <mikiyasu.obara@tebiki.co.jp>
2024-04-24 00:40:26 +0900

    feat: add MatrixContainer component

---
+++ src/components/MatrixContainer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="matrix-container">
+    <div>空タグ</div>
+    <slot name="user-header"></slot>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  users: string;
+  skills: string;
+}
+
+const props = defineProps<Props>();
+
+const horizontalLength = props.users.length + 1;
+
+console.log(props.users);
+</script>
+
+<style lang="scss" scoped>
+.matrix-container {
+  display: grid;
+  grid-template-columns: repeat(v-bind(horizontalLength), 1fr);
+  gap: 1rem;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,46 @@
 <template>
   <div class="container">
-    <div class="text-center">
-      <img alt="Vue logo" src="@/assets/logo.png" class="text-center" />
-    </div>
-    <HelloWorld msg="Welcome to Vue.js 3 Vite starter template" />
+    <MatrixContainer :users="users" :skills="skills">
+      <!-- <template #user-header> </template>
+           <template #skill-header> </template>
+           <template #content> </template> -->
+      <template #user-header>
+        <div v-for="user in users" :key="user.id">
+          {{ user.name }}
+        </div>
+      </template>
+      <div v-for="skill in skills" :key="skill.id">
+        {{ skill.name }}
+      </div>
+    </MatrixContainer>
   </div>
 </template>
 
-<script lang="ts">
-export default {
-  name: 'Home'
-};
+<script setup lang="ts">
+import MatrixContainer from '@/components/MatrixContainer.vue';
+
+interface User {
+  id: number;
+  name: string;
+}
+
+interface Skill {
+  id: number;
+  name: string;
+}
+
+const users: User[] = [
+  { id: 1, name: 'Alice' },
+  { id: 2, name: 'Bob' },
+  { id: 3, name: 'Charlie' }
+];
+
+const skills: Skill[] = [
+  { id: 1, name: 'JavaScript' },
+  { id: 2, name: 'TypeScript' },
+  { id: 3, name: 'Vue.js' }
+];
 </script>
 
-<script setup lang="ts">
-// https://github.com/vuejs/eslint-plugin-vue/issues/1577
-// eslint-disable-next-line import/first
-import HelloWorld from '@/components/HelloWorld.vue';
-</script>
+<style lang="scss" scoped>
+</style>
