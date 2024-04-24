c2c8f485192b6e4492cf16352f40ea35bb5a407c
Mikiyasu Obara <mikiyasu.obara@tebiki.co.jp>
2024-04-25 00:01:44 +0900

    feat: ユーザー名とスキル名を表示する

--- src/components/MatrixContainer.vue
+++ src/components/MatrixContainer.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="matrix-container">
     <div class="blank">空タグ</div>
-    <slot name="user-header"></slot>
-    <slot></slot>
+    <div v-for="user in props.users" :key="user.id" class="user-header">
+      <div class="user-header-item">{{ user.name }}</div>
+    </div>
+    <div class="content-wrapper">
+      <div v-for="skill in props.skills" :key="skill.id" class="content-row">
+        <div class="skill-header-item">
+          {{ skill.name }}
+        </div>
+        <div v-for="user in props.users" :key="user.id" class="content-cell">
+          <slot name="evaluation"></slot>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -31,11 +42,11 @@ const horizontalLength = props.users.length;
 .matrix-container {
   width: 100%;
   display: grid;
-  grid-template-columns: 200px repeat(v-bind(horizontalLength), minmax(auto, max-content));
+  grid-template-columns: max-content repeat(v-bind(horizontalLength), minmax(auto, max-content));
   gap: 1rem;
 
   > :not(:first-child) {
-    max-width: 100px;
+    /* max-width: 200px; */
     overflow: hidden;
     text-overflow: ellipsis;
     white-space: nowrap;
@@ -46,8 +57,23 @@ const horizontalLength = props.users.length;
   grid-column-start: 1;
 }
 
-.skill-header {
+.user-header {
   display: contents;
+}
+
+.content-wrapper {
+  display: contents;
+}
+
+.content-row {
+  display: contents;
+}
+
+.skill-header-item {
+  grid-column-start: 1;
+}
+
+.content-cell {
 }
 
 ::v-slotted(div) {
